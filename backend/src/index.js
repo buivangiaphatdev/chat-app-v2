3673dd186f8795a1e897f31af1317ec6173eac25
buivangiaphatdev <buivangiaphat@gmail.com>
2025-02-25 01:19:32 +0700

    set up run build

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,16 +3,19 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import path from "path";
+
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
-import {app, server} from "./lib/socket.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
+const _dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -25,6 +28,14 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(_dirname, "../frontend/dist")));
+
+  app.get("*",(req,res)=>{
+    res.sendFile(path.join(_dirname,"../frontend", "dist", "index.html"));
+  })
+}
 
 server.listen(PORT, () => {
   console.log("Server is running on PORT:" + PORT);
